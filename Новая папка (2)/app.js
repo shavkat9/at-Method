9b7d8733cebbf14ebdc8f2e4bdf711e98a098913
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2023-04-11 17:20:55 +0500

    Method AT

---
+++ Новая папка (2)/app.js
@@ -0,0 +1,17 @@
+// let a = 40;
+// let b = 50;
+// let g = 30;
+
+// let c = a - b + g;
+
+// console.log(c)
+
+// let a = 10;
+// let b = a;
+// a = 20
+// console.log(a, b)
+const fruits = ["Banana", "Orange", "Apricot", "Mango"];
+
+let fruit = fruits.at(2);
+
+console.log(fruit)
